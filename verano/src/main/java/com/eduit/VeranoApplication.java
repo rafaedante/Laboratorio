b1e7d5f7cdc4b392bf915ef651e84f960ca1019b
Rafael Ibarra <rafael.dante@gmail.com>
2022-01-22 19:40:40 -0300

    labo

---
+++ verano/src/main/java/com/eduit/VeranoApplication.java
@@ -0,0 +1,13 @@
+package com.eduit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeranoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeranoApplication.class, args);
+	}
+
+}
